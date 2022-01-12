430973944d32551ae3ee66c9b95e819e045399c7
Gonçalo Costa <94967933+Costa-atsoC@users.noreply.github.com>
2022-01-12 00:22:47 +0000

    Update BatalhaNaval.c

--- BatalhaNaval.c
+++ BatalhaNaval.c
@@ -1,9 +1,9 @@
 /**
  * @autores:
  * Nome: Gonçalo Costa      Número de Aluno: 48243
- * Nome:                    Número de Aluno:
- * Nome:                    Número de Aluno:
- **/ 
+ * Nome: Duarte Neves       Número de Aluno: 48030
+ * Nome: Miguel Marques     Número de Aluno: 48011
+ **/
 
 #include <stdio.h>
 #include <string.h>
@@ -23,35 +23,33 @@ typedef struct
 /**Associa cada coordenada do barco com um estado**/
 typedef struct
 {
-    int afloat;        //0 indica que está afundada, 1 indica que não
-    Position pos;      //Coordenada
+    int afloat;   //0 indica que está afundada, 1 indica que não
+    Position pos; //Coordenada
 } StateCoord;
 
 /**Representa um barco**/
 typedef struct
 {
-    int afloat;        //Posições que ainda não foram afundadas
-    int tSize;         //Tamanho do tipo do barco 
+    int afloat;          //Posições que ainda não foram afundadas
+    int tSize;           //Tamanho do tipo do barco
     StateCoord coord[5]; //O barco maior tem 5 coordenadas, usando o tSize garantimos que acedemos apenas às existentes
-    char type;         //Caracter que representa o tipo do barco
+    char type;           //Caracter que representa o tipo do barco
 } Boat;
 
 /**Representa um tabuleiro**/
 typedef struct
 {
     int numBoatsAfloat; //Número de barcos não afundados
-    int numBoats; //Número de barcos que foram colocados
-    Boat boats[B]; //Array de barcos. Cada barco é do tipo Boat.
-    char board[N][M]; //Array que contém a informação de cada posição do tabuleiro
+    int numBoats;       //Número de barcos que foram colocados
+    Boat boats[B];      //Array de barcos. Cada barco é do tipo Boat.
+    char board[N][M];   //Array que contém a informação de cada posição do tabuleiro
 } Board;
-
 
 /**
  * NOTA IMPORTANTE: 
  * Não pode alterar nenhuma das assinaturas das funções abaixo. 
  * Este ficheiro tem de ser considerado em conjunção com o enunciado do TP2.
  **/
-
 
 /**
  * Function: init_board
@@ -72,7 +70,7 @@ void init_board(int n, int m, Board *b)
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
-        { 
+        {
             b->board[i][j] = ' ';
         }
     }
@@ -105,7 +103,7 @@ void print_board(int n, int m, char board[n][m], int flag)
     for (int i = 0; i < m; i++)
     {
         printf("|");
-        if (flag) //Mostra tudo
+        if (flag == 1) //Mostra tudo
         {
             for (int j = 0; j < n; j++)
             {
@@ -115,7 +113,11 @@ void print_board(int n, int m, char board[n][m], int flag)
         }
         else // Esconde os barcos
         {
-            //Implementar
+            for (int j = 0; j < n; j++)
+            {
+                printf("   |");
+            }
+            printf("\n");
         }
 
         printf("+");
@@ -126,7 +128,6 @@ void print_board(int n, int m, char board[n][m], int flag)
         printf("\n");
     }
 }
-
 
 /** 
  * Function: typeToSize
@@ -142,23 +143,23 @@ void print_board(int n, int m, char board[n][m], int flag)
 int typeToSize(char type)
 {
     int tamanho;
-    switch(toupper(type))
+    switch (toupper(type))
     {
-        case 'P':
-            tamanho = 5;
-            break;
-        case 'N':
-            tamanho = 4;
-            break;
-        case 'C':
-            tamanho = 3;
-            break;
-        case 'S':
-            tamanho = 2;
-            break;
-        default:
-            tamanho = -1;
-            break;
+    case 'P':
+        tamanho = 5;
+        break;
+    case 'N':
+        tamanho = 4;
+        break;
+    case 'C':
+        tamanho = 3;
+        break;
+    case 'S':
+        tamanho = 2;
+        break;
+    default:
+        tamanho = -1;
+        break;
     }
 
     return tamanho;
@@ -186,9 +187,22 @@ int typeToSize(char type)
  **/
 void init_boat(Boat *b, char type, Position xy, char dir)
 {
-    //Implementar
+    b->tSize = typeToSize(type);
+    b->afloat = typeToSize(type);
+    for(int j = 0 ; j < b->tSize; j++)
+    {
+        if (toupper(dir) == 'H')
+        {
+            b->coord[j].pos.x += j;
+            b->coord[j].afloat = 1;
+        }
+        if(toupper(dir) == 'V')
+        {
+            b->coord[j].pos.y += j;
+            b->coord[j].afloat = 1;
+        }
+    }
 }
-
 
 /**
  * Function: check_free
@@ -208,9 +222,9 @@ void init_boat(Boat *b, char type, Position xy, char dir)
  **/
 int check_free(int n, int m, Boat *boat, char board[n][m])
 {
-   //Implementar
+    //Implementar
 
-   return -1;
+    return -1;
 }
 
 /** 
@@ -291,42 +305,44 @@ int target(int x, int y, Board *board)
 //int colocaNavio()
 int main(void)
 {
-    Boat player1[6];
-    Boat player2[6];
-    Position lugar[50];
-    char type;
-    int posx, posy
+    Boat player1[B];
+    Boat player2[B];
+    Position lugar[64];
+    char type, dir;
+    int posx, posy;
 
     printf("---------------------------------BATALHA NAVAL---------------------------------\n\n");
     printf("*Informações importantes:\n");
-    printf("*\t·")
-    
-    for(int i = 0; i < 6; i+=2)
+    printf("*\t·");
+
+    for (int i = 0; i < B; i++)
     {
         printf("--------------Player 1--------------\n");
-        printf("\tBarco nº%d:", i+1);
+        printf("\tBarco nº%d\n", i + 1);
         printf("\t\t·Indique o tipo de barco:");
         scanf("%c", &type); //P tem 5 casas, N tem 4, C tem 3, S tem 2
-        typeToSize(type);
+        getchar();
         printf("\t\t·Indique a 1ª posição: ");
-        scanf("(%d, %d)", &posx, &posy);
-        lugar.x = posx;
-        lugar.y = posy;
-        printf("Indique a direção do barco")
-        init_boat(player1[i], type, lugar, dir);
+        scanf("%d %d", &posx, &posy);
+        lugar->x = posx;
+        lugar->y = posy;
+        getchar();
+        printf("Indique a direção do barco: \n");
+        scanf(" %c", &dir);
+        getchar();
+        init_boat(&player1[i], type, *lugar, dir);
 
-        init_boat(player2[i]);
+        //init_boat(player2[i]);
     }
-    
+
     Board brd;
     init_board(N, M, &brd);
     print_board(N, M, brd.board, 1);
-    
+
     /**Exemplo de uso da print_board e da place_boat**/
     /**Precisa de as implementar primeiro**/
     //print_board(N, M, brd.board, 0);
     //place_boat(1,3, 'H', 'P', &brd);
-    
-    
+
     return 0;
 }
